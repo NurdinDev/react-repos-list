f6196c289dc57960fbe4037696832a5be97ef77a
NurdinDev <nureddinbedevi@gmail.com>
2021-01-09 10:31:19 +0300

    Use the custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { gql } from "graphql-request";
+import { useGraphQuery } from "./hooks/useGraphQuery";
 
-function App() {
-  return (
-    <div className="App">
-     Hello
-    </div>
+const GET_REACT_REPOS = gql`
+  query() {
+    search(query: "topic:react", type: REPOSITORY, first: 10) {
+      repositoryCount
+      pageInfo {
+        startCursor
+        hasNextPage
+        endCursor
+        hasPreviousPage
+      }
+      nodes {
+        ... on Repository {
+          name
+          forkCount
+          stargazerCount
+          url
+        }
+      }
+    }
+  }
+`;
+
+const App = () => {
+  const { data, isLoading, error } = useGraphQuery(
+    "search",
+    GET_REACT_REPOS
   );
-}
+  console.log(data);
+  return <p>Hello</p>;
+};
 
 export default App;
